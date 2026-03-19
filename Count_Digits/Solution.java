2ebbd5e31ee24ab2935a7c0c8f23d2768c9e0938
rithik <rithikraja61@gmail.com>
2026-03-19 20:44:25 +0530

    create folder

---
+++ Count_Digits/Solution.java
@@ -0,0 +1,15 @@
+package Count_Digits;
+
+class Solution {
+    public int countDigits(int n) {
+        // code here
+        int count=0;
+
+        while(n!=0)
+        {
+            n=n/10;
+            count++;
+        }
+        return count;
+    }
+}
