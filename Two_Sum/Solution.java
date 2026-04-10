6e05117b5520f23d2e3fb46b563485504a4668ff
rithik <rithikraja61@gmail.com>
2026-04-10 23:30:13 +0530

    Two_Sum

---
+++ Two_Sum/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] sol=new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<nums.length;j++)
+            {
+                if(i==j)
+                {
+                    continue;
+                }
+                if(nums[i]+nums[j]==target)
+                {
+                    sol[0]=i;
+                    sol[1]=j;
+                    break;
+
+                }
+            }
+        }
+        return sol;
+    }
+}
