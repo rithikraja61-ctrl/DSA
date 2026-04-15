21b51d3bd9ebc483fdd4b4d83a7d8801a7db58ab
rithik <rithikraja61@gmail.com>
2026-04-15 10:37:53 +0530

    First Unique Character Using Hash Map

---
+++ First_Unique_Character_Using_Hash_Map/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer> hash=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(hash.containsKey(s.charAt(i)))
+            {
+                hash.put(s.charAt(i),hash.get(s.charAt(i))+1);
+            }
+            else
+            {
+                hash.put(s.charAt(i),1);
+            }
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(hash.get(s.charAt(i))==1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
