65d38f3f246b8043b6782c80c162995a5f85cf0b
rithik <rithikraja61@gmail.com>
2026-04-20 10:06:54 +0530

    Search insert position

---
+++ Search_Insert_Position/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        int mid=0;
+        while(i<=j)
+        {
+            mid=(i+j)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]<target)
+            {
+                i=mid+1;
+            }
+            else
+            {
+                j=mid-1;
+            }
+        }
+        if(i==0&&nums[mid]>target)
+        {
+            System.out.println(nums[mid]);
+            return 0;
+        }
+
+        return i;
+    }
+}
