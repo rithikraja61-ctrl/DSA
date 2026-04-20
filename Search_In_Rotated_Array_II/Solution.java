cd7320ac127bc8e2be7a6c376e372c95ab921e8d
rithik <rithikraja61@gmail.com>
2026-04-20 13:17:46 +0530

    Search in Rotated Array II

---
+++ Search_In_Rotated_Array_II/Solution.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        int mid=0;
+        while(i<=j)
+        {
+            mid=(i+j)/2;
+            if(nums[mid]==target)
+            {
+                return true;
+            }
+            else if(nums[i]==nums[mid]&&nums[mid]==nums[j])
+            {
+                i++;
+                j--;
+                continue;
+            }
+            else if(nums[mid]>=nums[i])
+            {
+                if(target>=nums[i]&&target<=nums[mid])
+                {
+                    j=mid-1;
+                }
+                else
+                {
+                    i=mid+1;
+                }
+            }
+            else
+            {
+                if(target>=nums[mid]&&target<=nums[j])
+                {
+                    i=mid+1;
+                }
+                else
+                {
+                    j=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
